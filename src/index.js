abed0925cd05bf0bced5d761b56bbdc148604521
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-10-25 13:31:33 +0300

    feat: implement a solution of human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const numbers = [
+        '',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+        'twenty'
+    ];
+
+    const tens = [
+        '',
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+    if (!number) {
+        return 'zero';
+    }
+
+    let result = [];
+    let degree;
+
+    if (number > 99) {
+        degree = Math.floor(number / 100);
+        result.push(`${numbers[degree]} hundred `);
+        number = number - degree * 100;
+    }
+
+    if (number > 19) {
+        degree = Math.floor(number / 10);
+        number = number - degree * 10;
+        result.push(`${tens[degree]} `);
+        result.push(`${numbers[number]} `);
+    } else {
+        result.push(numbers[number]);
+    }
+
+    return result.join(``).trim();
+
 }
